9a78e67a40ce0476f473e58a5aadcb9dee92ac0c
Dawnıng <144d.reaming@gmail.com>
2026-09-05 22:35:56 +0900

    update update.bat to install dependencies if virtual environment is missing

--- update.bat
+++ update.bat
@@ -1,5 +1,5 @@
 @echo off
 
-ssh user@192.168.219.122 "cd ~/apps/Reaminal && git pull && sudo systemctl restart reaminal"
+ssh user@192.168.219.122 "cd ~/apps/Reaminal && git pull && (test -d venv || python3 -m venv venv) && ./venv/bin/pip install -r requirements.txt && sudo systemctl restart reaminal"
 
 pause
